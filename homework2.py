35fc7b2c68e2c194cbb2a39304b81a92380839b3
TagirovTimur <Attteupoh@gmail.com>
2024-07-24 22:49:29 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+Homework_total = 12
+Total_hours = 1.5
+Course_Name = 'Python'
+Time_for_one_task = Total_hours / Homework_total
+print('Курс:'  + Course_Name + ',','затрачено часов: '+ str(Total_hours) +',',  'среднее время выполнения ' + str(Time_for_one_task) +' часа')
